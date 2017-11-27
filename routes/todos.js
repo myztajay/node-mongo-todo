fbe24a68acafdab2301f9f3b6e7aefbe9fcff72b
myztajay <myztajay@hotmail.com>
2017-11-27 13:52:53 -0500

    Added update method in routes file

--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require ('../todos_api/models')
 
-
+// INDEX
 router.get('/',(req,res)=>{
   db.Todo.find()
   .then((todos)=>{
@@ -17,6 +17,26 @@ router.post('/', (req,res)=>{
   db.Todo.create(req.body)
   .then((newTodo)=>{
     res.status(201).json(newTodo)
+  })
+  .catch((err)=>{
+    res.send(err)
+  })
+})
+//SHOW
+router.get('/:todoId', (req, res)=>{
+  db.Todo.findById(req.params.todoId)
+  .then((foundTodo)=>{
+    res.json(foundTodo)
+  })
+  .catch((err)=>{
+    res.send(err)
+  })
+})
+//UPDATE
+router.put('/:todoId', (req, res)=>{
+  db.Todo.findOneAndUpdate({_id: req.params.todoId},  req.body, {new: true})
+  .then((updatedTodo)=>{
+    res.json(updatedTodo)
   })
   .catch((err)=>{
     res.send(err)
